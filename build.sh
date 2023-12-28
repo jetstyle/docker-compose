6ae1b8006178e3c5cb1746c9ecaf0918e88de8ea
Karkh <pavelkarkh@gmail.com>
2023-12-28 18:13:08 +0500

    update build section

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 TAGNAME="docker-compose"
+ACCOUNT="jetstylehub"
 VESRION=3.17
 
 docker buildx build --platform linux/amd64 -t $TAGNAME .
 
-docker tag $TAGNAME gnextapp/$TAGNAME:$VESRION
-docker tag $TAGNAME gnextapp/$TAGNAME:latest
+docker tag $TAGNAME $ACCOUNT/$TAGNAME:$VESRION
+docker tag $TAGNAME $ACCOUNT/$TAGNAME:latest
 
-docker push gnextapp/$TAGNAME:$VESRION
-docker push gnextapp/$TAGNAME:latest
+docker push $ACCOUNT/$TAGNAME:$VESRION
+docker push $ACCOUNT/$TAGNAME:latest
